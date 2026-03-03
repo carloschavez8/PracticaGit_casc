db925b78af9cdf8842efa048d01ea903558e5a1f
alber <alber@DESKTOP-TIRQVA8>
2026-03-03 07:41:28 -0600

    feat: estructura inicial del proyecto.

---
+++ src/main/java/com/mycompany/practicagit_casc/PracticaGit_casc.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicagit_casc;
+
+/**
+ *
+ * @author alber
+ */
+public class PracticaGit_casc {
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de gestión");
+        
+   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+   }
+}
